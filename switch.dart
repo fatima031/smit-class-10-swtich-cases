304d68a04bb46aa08dfc03f6513c942deb0d7033
fatima <fatima031>
2024-02-01 02:02:47 +0500

    switch casses

--- switch.dart
+++ switch.dart
@@ -1,3 +1,23 @@
-void main(){
-  
+import 'dart:io';
+
+void main() {
+  num num1 = 3;
+  num num2 = 4;
+
+  var userInput = stdin.readLineSync();
+  switch (userInput) {
+    case "+":
+      print(num1 + num2);
+      break;
+    case "-":
+      print(num1 - num2);
+      break;
+    case "*":
+      print(num1 * num2);
+      break;
+    case "/":
+      print(num1 / num2);
+    default:
+      print("Unsupported oprations");
+  }
 }
